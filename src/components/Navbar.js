ae99abed7d9b6c157c9ca481903e0631eec2d3cc
Catrina-W <catrinawhaley19@gmail.com>
2025-01-01 18:17:53 -0500

    Routed new pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ import {Link} from "react-router-dom";
 class Navbar extends Component {
     state = {clicked: false};
     handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
+        this.setState({ clicked: !this.state.clicked})
     }
-    render(){
-        return(
+    render() {
+        return (
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Trippy</h1>
 
@@ -17,13 +17,13 @@ class Navbar extends Component {
                     <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
 
-                <ul className={this.state.clicked ? ".nav-menu active" : ".nav-menu"}>
-                    {MenuItems.map((item, index) =>{
-                        return(
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return (
                             <li key={index}>
-                                <a className={item.cName} href="/">
+                                <Link className={item.cName} to={item.url}>
                                     <i className={item.icon}></i>{item.title}
-                                </a>
+                                </Link>
                             </li>
                         );
                     })}
