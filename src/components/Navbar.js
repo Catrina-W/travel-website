040b5c952be247e63a010a1207784a2807acb5fb
Catrina-W <catrinawhaley19@gmail.com>
2025-01-01 15:34:17 -0500

    created responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,30 @@ import { MenuItems } from "./MenuItems";
 import {Link} from "react-router-dom";
 
 class Navbar extends Component {
+    state = {clicked: false};
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Trippy</h1>
 
-                <ul className="nav-menu">
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+
+                <ul className={this.state.clicked ? ".nav-menu active" : ".nav-menu"}>
                     {MenuItems.map((item, index) =>{
                         return(
-                            <li>
-                                <a href="/">
-                                    <i className="fa-solid fa-house"></i><Link>Home</Link>
+                            <li key={index}>
+                                <a className={item.cName} href="/">
+                                    <i className={item.icon}></i>{item.title}
                                 </a>
                             </li>
                         );
                     })}
-
+                    <button>Sign Up</button>
                 </ul>
             </nav>
         );
