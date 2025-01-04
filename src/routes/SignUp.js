e0e5e1603d83e06b6711a83c8432ab34a52eb40f
Catrina-W <catrinawhaley19@gmail.com>
2025-01-04 11:51:43 -0500

    Changed Sign Up hero image

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -1,6 +1,6 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
-import AboutImg from "../assets/2.jpg";
+import SignUpImg from "../assets/10.jpg";
 import Footer from "../components/Footer";
 import SignUpForm from "../components/SignUpForm";
 
@@ -10,7 +10,7 @@ function SignUp() {
             <Navbar/>
             <Hero
                 cName="hero-mid"
-                heroImg={AboutImg}
+                heroImg={SignUpImg}
                 title="Sign Up"
                 btnClass="hide"
             />
