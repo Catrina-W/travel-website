f1ff4bee4ffaf6fb820b5656361ec4e753c2852d
Catrina-W <catrinawhaley19@gmail.com>
2025-01-04 11:38:40 -0500

    Completed Contact Form

---
+++ src/components/AboutUs.js
@@ -0,0 +1,16 @@
+import "./AboutUsStyles.css";
+
+function AboutUs () {
+    return (
+        <div className="about-container">
+            <h1>Our History</h1>
+            <p>Trippy is owned and managed by Trippy Inc., a leading brand in web design services and e-commerce solutions</p>
+            <h1>Our Mission</h1>
+            <p>Our mission is to successfully meet the requirements of our clients and reach the horizon of our capabilities</p>
+            <h1>Our Vision</h1>
+            <p>To sow the seeds of excellent service with customer centric approaches</p>
+        </div>
+    );
+}
+
+export default AboutUs;
